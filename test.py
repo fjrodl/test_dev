79f2a92ed1696f7ee4da0cc009a86b4e42da03e4
fjrodl <fjrodl>
2024-11-27 11:17:06 +0100

    ADded issue in python file for checking pylint action

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
+"""
+Aplicacion 0 de test, no utilizar
+"""
 from app import app
 
-def test_home():
-    # REalizamos la prueba
-    response = app.test_client().get("/")
-    
-    assert response.status_code==200  # Check for HTTP 200 status
-    assert response.data==b"Hello, Flask!"  # Check the response content
+
+    def test_home():
+        """
+        test basico del servidor flask
+        """
+        # REalizamos la prueba
+        response = app.test_client().get("/")
+
+        assert response.status_code == 200  # Check for HTTP 200 status
+        assert response.data == b"Hello, Flask!"  # Check the response content
