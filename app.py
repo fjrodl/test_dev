35f759f11db2a176ec562e7bc3bbf21efad64203
fjrodl <fjrodl@users.noreply.github.com>
2025-10-08 16:28:46 +0200

    Cambios menores al finchero de la web

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 # app.py
-"""
-Ejemplo básico de web utilizando Flask
-"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    """
-    Función base
-    """
     return "Hello, CI/CD with Docker! Miercoles 27"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
