6e767f8bf9a9b39227ddaeb985569c4239365e80
fjrodl <fjrodl@users.noreply.github.com>
2025-10-15 12:45:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-"""
-Aplicacion 0 de test, no utilizar
-"""
 from app import app
 
 
 def test_home():
-    """
-        test basico del servidor flask
-        """
-    # REalizamos la prueba
     response = app.test_client().get("/")
 
     assert response.status_code == 200  # Check for HTTP 200 status
